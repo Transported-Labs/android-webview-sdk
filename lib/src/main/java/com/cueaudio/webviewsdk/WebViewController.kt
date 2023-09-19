4fcd8f32f5d81fcea5cbcefd4e662ab32f0bbbe6
Alexander Mokrushin <amokrushin@transportedlabs.com>
2023-09-19 11:04:19 +0300

    Added openInBrowser method.

--- lib/src/main/java/com/cueaudio/webviewsdk/WebViewController.kt
+++ lib/src/main/java/com/cueaudio/webviewsdk/WebViewController.kt
@@ -2,6 +2,7 @@ package com.cueaudio.webviewsdk
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.webkit.URLUtil
 
 class InvalidUrlError(message: String): Exception(message)
@@ -20,4 +21,17 @@ class WebViewController(private val mContext: Context) {
         }
     }
 
+    ///Checks validity of passed URL, navigates to the url in default browser
+    @Throws(InvalidUrlError::class)
+    fun openInBrowser(url: String) {
+        if (URLUtil.isValidUrl(url)) {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            if (intent.resolveActivity(mContext.getPackageManager()) != null) {
+                mContext.startActivity(intent)
+            }
+        } else {
+            throw InvalidUrlError("Invalid URL: '$url'")
+        }
+    }
 }
