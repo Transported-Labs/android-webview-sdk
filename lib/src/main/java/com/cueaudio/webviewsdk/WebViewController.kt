506de995687db60975364142361b1c20ffc29eb7
Alexander Mokrushin <amokrushin@transportedlabs.com>
2023-09-20 10:58:55 +0300

    Changed openInBrowser to openInChrome method to navigate to url in Chrome CustomTabs.

--- lib/src/main/java/com/cueaudio/webviewsdk/WebViewController.kt
+++ lib/src/main/java/com/cueaudio/webviewsdk/WebViewController.kt
@@ -2,12 +2,15 @@ package com.cueaudio.webviewsdk
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.webkit.URLUtil
+import androidx.browser.customtabs.CustomTabsIntent
 
 class InvalidUrlError(message: String): Exception(message)
 
 class WebViewController(private val mContext: Context) {
+    private val chromeAppID = "com.android.chrome"
 
     ///Checks validity of passed URL, starts new activity, navigates to the url in embedded WebView-object
     @Throws(InvalidUrlError::class)
@@ -21,17 +24,34 @@ class WebViewController(private val mContext: Context) {
         }
     }
 
-    ///Checks validity of passed URL, navigates to the url in default browser
+    ///Checks validity of passed URL, navigates to the url in Chrome Custom Tabs
     @Throws(InvalidUrlError::class)
-    fun openInBrowser(url: String) {
+    fun openInChrome(url: String) {
         if (URLUtil.isValidUrl(url)) {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            if (intent.resolveActivity(mContext.getPackageManager()) != null) {
-                mContext.startActivity(intent)
+            if (isAppInstalled(chromeAppID)) {
+                    val intentCustomTabs = CustomTabsIntent.Builder()
+                        .setUrlBarHidingEnabled(true)
+                        .setShowTitle(false).build()
+                    intentCustomTabs.intent.setPackage(chromeAppID);
+                    intentCustomTabs.intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intentCustomTabs.launchUrl(mContext, Uri.parse(url))
+            } else {
+                throw InvalidUrlError("Browser is not installed: '$chromeAppID'")
             }
         } else {
             throw InvalidUrlError("Invalid URL: '$url'")
         }
+    }
+    @Suppress("DEPRECATION")
+    private fun isAppInstalled(packageName: String): Boolean {
+        val pm: PackageManager = mContext.packageManager
+        var installed = false
+        installed = try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+        return installed
     }
 }
